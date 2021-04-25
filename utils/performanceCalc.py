633b070939d035e88d64a59e4e20a5da3597e107
Eric Lee <macbookpro4eric@Erics-MacBook-Pro.local>
2021-04-25 19:46:13 +0900

    Add utils.performanceCalc.py to monitor performance

---
+++ utils/performanceCalc.py
@@ -0,0 +1,12 @@
+import json
+import pandas
+import os
+
+base_dir = os.path.abspath(os.path.dirname(__name__))
+data_dir = os.path.join(base_dir, "arbitrageHistory.json")
+
+if __name__ == "__main__":
+    with open(data_dir) as file:
+        histData = json.load(file)
+        df_histData = pandas.DataFrame(histData)
+        df_histData.to_excel(os.path.join(base_dir, "utils/outputData/performanceSummery.xlsx"))
